a5fb328ad7a307443c78b8b48a38d90f01de5ae0
PriyaDiswar <95042955+PriyaDiswar@users.noreply.github.com>
2023-05-05 00:39:00 +0530

    Create Text_Detection.py
    Detect the text written on the image and show it as an output.

---
+++ Text_Detection.py
@@ -0,0 +1,18 @@
+!pip install easyocr
+
+import easyocr
+import cv2
+import matplotlib.pyplot as plt
+
+img = cv2.imread("/download (2).jpg")    #file path in quotes
+
+plt.imshow(img);
+plt.show()
+
+read=easyocr.Reader(['en'])
+
+read.readtext(img)
+
+data=read.readtext(img)
+for i in data:
+  print(i[1])
